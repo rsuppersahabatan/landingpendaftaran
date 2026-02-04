3d9ee8c2c78268cff5a32d7d67b678a7f34c2e0c
mdestafadilah <desta.08b@gmail.com>
2026-02-04 11:55:19 +0700

    test

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -92,9 +92,11 @@ const StyledPostListItem = styled.li`
   transition: all 0.3s ease-out;
 
   body.light-mode & {
+    -webkit-backdrop-filter: blur(10px);
     backdrop-filter: blur(10px);
     border: 1px solid rgba(255, 255, 255, 0.5);
-    background-color: rgba(255, 255, 255, 0.3);
+    background-color: rgba(255, 255, 255, 0.2);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   }
 
   body.light-mode &:hover {
